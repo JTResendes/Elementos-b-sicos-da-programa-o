333d6ca0298624b5f6f1a6fdd9ad4d807a75943e
JoaoResendes <93650121+JoaoResendes@users.noreply.github.com>
2022-10-02 21:17:54 +0100

    incompleto

---
+++ ex3.py
@@ -0,0 +1,12 @@
+""""
+ # Um minuto tem 60 segundos
+# Uma hora tem 3600 (60 * 60) segundos
+# Um dia tem 24 horas, logo 24 * 3600 segundos"""
+
+if __name__ == '__main__':
+
+
+ tempo = int(input(f"Escreva um número de segueing."te)
+ segundos = 24 * 3600 * 3600 * 60
+
+    print("Dia:"(segundos))
