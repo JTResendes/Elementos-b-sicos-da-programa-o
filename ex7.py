9010a13401d2c6cc9ec07d54eca5d604ea161cbc
JoaoResendes <93650121+JoaoResendes@users.noreply.github.com>
2022-10-03 20:51:37 +0100

    ex7 duvida

--- ex7.py
+++ ex7.py
@@ -3,12 +3,16 @@
 if __name__ == '__main__':
     horas = int(input("Digite o numero de horas"))
     sporhora = int(input("Salário por hora"))
+    n = input("Horas extras")
+    n1 = n * 2
 
 if horas < 40:
     salario = sporhora * horas
     print(f'Salário {salario}')
+
+
 else:
     horas > 40
-    salario = (sporhora * horas)
+    salario = (sporhora * horas) * n1
 
     print(f'Salário {salario}')
