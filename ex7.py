aa23c6cb2157e8b75daa5e0ae2e03e5f82846472
JoaoResendes <93650121+JoaoResendes@users.noreply.github.com>
2022-10-03 21:18:04 +0100

    ex7 feito, ver novamente

--- ex7.py
+++ ex7.py
@@ -4,15 +4,41 @@ if __name__ == '__main__':
     horas = int(input("Digite o numero de horas"))
     sporhora = int(input("Salário por hora"))
     n = input("Horas extras")
-    n1 = n * 2
+
+
 
 if horas < 40:
     salario = sporhora * horas
-    print(f'Salário {salario}')
+    extra = 0
+    print(f'Salário {salario + extra}')
 
 
 else:
     horas > 40
-    salario = (sporhora * horas) * n1
+    salario = (sporhora * horas)
+    extra = (horas - 40) * (sporhora * 2)
 
-    print(f'Salário {salario}')
+    print(f'Salário {salario + extra}')
+
+"""
+def salario(num1, num2):
+    total = 0
+    if num1 <= 40:
+        salario = num1 * num2
+        extra = 0
+        total = salario + extra
+    if num1 > 40:
+        salario = (num1 * num2) - ((num1 - 40) * num2)
+        extra = (num1 - 40) * (num2 * 2)
+        total = salario + extra
+    return total
+
+
+while __name__ == '__main__':
+    continuar = 's'
+    while continuar == 's':
+        num1 = int(input('Insira as horas: '))
+        num2 = int(input('Insira os preço/hora: '))
+        print(f'O salário de {num1} horas é {salario(num1, num2)}')
+        continuar = input('Quer continuar? [s, n] ')
+    print('Adeus!')"""
---
+++ ex8.py
@@ -0,0 +1,8 @@
+num = 0
+cont = 0
+
+while num >= 0:
+    num = int(input('Digite um numero'))
+    cont += 1
+print("Fim".format(cont))
+
